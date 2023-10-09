ea15e6276683321817cef2bd7ecb510ea94389b6
TrungDev <thanhtrungabcxyz@gmail.com>
2023-10-09 19:56:00 +0700

    save

--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Model/UserAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vn.BackEnd_Job_Website.Model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
-}
--- BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Service/impl/AccountServiceImpl.java
+++ BackEnd_Job_Website/src/main/java/com/vn/BackEnd_Job_Website/Service/impl/AccountServiceImpl.java
@@ -2,14 +2,11 @@ package com.vn.BackEnd_Job_Website.Service.impl;
 
 import com.vn.BackEnd_Job_Website.Dto.AccountDTO;
 import com.vn.BackEnd_Job_Website.Model.UserAccount;
-import com.vn.BackEnd_Job_Website.Model.UserAccountRepository;
 import com.vn.BackEnd_Job_Website.Respository.AccountRespository;
 import com.vn.BackEnd_Job_Website.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -18,8 +15,6 @@ public class AccountServiceImpl implements AccountService {
     private AccountRespository accountRespository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserAccountRepository userAccountRepository;
 
     @Override
     public String save(AccountDTO accountDTO) {
